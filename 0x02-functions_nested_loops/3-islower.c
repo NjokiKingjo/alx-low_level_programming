ff653312ea1eeff392f7ddcb8737d80a3a3437c4
NjokiKingjo <njoki.mburu101@gmail.com>
2023-02-21 15:51:29 -0500

    checks for lowercase character.

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,1 +1,10 @@
-#!/bin/bash
+#include "main.h"
+/**
+ * _islower - check for lower case letter
+ * @c : character to check the case
+ * Return:0 or 1
+ */
+int _islower(int c)
+{
+	return (c >= 97 && c <= 122);
+}
